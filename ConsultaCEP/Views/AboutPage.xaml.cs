34830a8c7581bcafecb1f8aab59db7484f9c0791
PALOMA AZEVEDO SANTOS <paloma.asantos17@senacsp.edu.br>
2023-01-12 22:03:41 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultaCEP/Views/AboutPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ConsultaCEP.Services;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConsultaCEP.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+        private void MetodoPesquisaCEP(object sender, EventArgs e)
+        {
+            PesquisaCEP pesquisaCEP = new PesquisaCEP();
+
+            ViaCEP oViacep = new ViaCEP();
+            oViacep = pesquisaCEP.Consulta(entry_cep.Text);
+
+            lbl_cep.Text = oViacep.cep;
+            lbl_logradouro.Text = oViacep.logradouro;
+            lbl_complemento.Text = oViacep.complemento;
+            lbl_bairro.Text = oViacep.bairro;
+            lbl_localidade.Text = oViacep.localidade;
+            lbl_uf.Text = oViacep.uf;
+            lbl_ibge.Text = oViacep.ibge;
+            lbl_gia.Text = oViacep.gia;
+            lbl_ddd.Text = oViacep.ddd;
+            lbl_siafi.Text = oViacep.siafi;
+        }
+    }
+}
---
+++ ConsultaCEP/Views/NewItemPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ConsultaCEP.Models;
+using ConsultaCEP.Services;
+using ConsultaCEP.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConsultaCEP.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+
+        private void MetodoPesquisaCEP(object sender, EventArgs e)
+        {
+            PesquisaCEP pesquisaCEP = new PesquisaCEP();
+
+            ViaCEP oViacep = new ViaCEP();
+            oViacep = pesquisaCEP.Consulta(entry_cep.Text);
+
+            lbl_logradouro.Text = oViacep.logradouro;
+        }
+    }
+}
